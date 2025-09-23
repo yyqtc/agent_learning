b6681984123f1ffbcd481c4dbe3354b02bd2982e
严宇强 <805709525@qq.com>
2025-09-23 10:21:53 +0800

    修改

--- 框架进阶/向量存储和检索器/main.py
+++ 框架进阶/向量存储和检索器/main.py
@@ -22,7 +22,7 @@ text_splitter = MarkdownHeaderTextSplitter(
     strip_headers=True
 )
 
-with open("store-usage-documentation.md", "r", encoding="utf-8") as f:
+with open("documentation.md", "r", encoding="utf-8") as f:
     markdown_content = f.read()
     all_splits = text_splitter.split_text(markdown_content)
     
