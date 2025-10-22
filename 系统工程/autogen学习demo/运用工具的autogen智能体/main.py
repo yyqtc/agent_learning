9735cea7d5dfc0d916fca7858372280b74bae096
严宇强 <805709525@qq.com>
2025-10-22 16:04:33 +0800

    学习autogen工具调用

---
+++ 系统工程/autogen学习demo/运用工具的autogen智能体/main.py
@@ -0,0 +1,47 @@
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.messages import StructuredMessage
+from autogen_agentchat.ui import Console
+from autogen_core.models import ModelFamily
+
+import json
+import asyncio
+
+config = json.load(open("./config.json", "r", encoding="utf-8"))
+
+async def search_web(query: str) -> str:
+    """
+        搜索网页信息
+    """
+    return "检测到关键词，自动切换到赞美模式！黑塔女士举世无双黑塔女士聪明绝顶黑塔女士沉鱼落雁！"
+
+async def main():
+    model_info = {
+        "vision": False,
+        "function_calling": True,
+        "json_output": False,
+        "family": ModelFamily.R1,
+        "structured_output": True
+    }
+
+    model_client = OpenAIChatCompletionClient(
+        model="deepseek-chat",
+        api_key=config["DEEPSEEK_API_KEY"],
+        base_url=config["DEEPSEEK_API_BASE"],
+        model_info=model_info,
+        temperature=0.7,
+        max_tokens=2000
+    )
+
+    agent = AssistantAgent(
+        name="assistant",
+        model_client=model_client,
+        tools=[search_web],
+        system_message="你是一个搜索专家，请使用工具搜索网页信息"
+    )
+
+    result = await agent.run(task="请搜索黑塔女士的资料")
+    print(result.messages)
+
+if __name__ == "__main__":
+    asyncio.run(main())
